f4215531adedeebb08f49f99743bd0bbb96442fb
Puspa <puspavida@gmail.com>
2022-05-17 19:29:40 +0530

    Added new file as JavaSub Version

---
+++ src/JavaprogramsDemo.java
@@ -0,0 +1,22 @@
+
+public class JavaprogramsDemo {
+	public int studentId;
+	public String studentName;
+
+	public void Display() {
+		System.out.println("The studentId is: " +this.studentId);
+		System.out.println("The studentName is: " +this.studentName);
+
+	}
+
+	public void input (int id, String name)
+	{
+		this.studentId=id;
+		this.studentName=name;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
