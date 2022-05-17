e1a76770a1d1a90c393331819b9416ed7a708c87
Puspa <puspavida@gmail.com>
2022-05-17 16:12:44 +0530

    Added new file in this application

---
+++ src/NewTest.java
@@ -0,0 +1,10 @@
+
+public class NewTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello_ World");
+		System.out.println("Hello_ World");
+	}
+
+}
--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,8 @@ public class Test {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
